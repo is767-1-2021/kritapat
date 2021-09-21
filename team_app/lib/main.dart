bef31cda1a6bf2ab319bf8eb50a24463a93ea076
Kritapat Pattarapititanont <kritapat-pat63@tbs.tu.ac.th>
2021-09-21 21:06:20 +0700

    midterm_app

--- team_app/lib/main.dart
+++ team_app/lib/main.dart
@@ -1,113 +1,268 @@
+import 'package:filter_list/filter_list.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(title: 'Filter list example'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  MyHomePage({Key? key, this.title}) : super(key: key);
+  final String? title;
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  List<User>? selectedUserList = [];
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  void _openFilterDialog() async {
+    await FilterListDialog.display<User>(
+      context,
+      listData: userList,
+      selectedListData: selectedUserList,
+      height: 480,
+      headlineText: "Select Users",
+      searchFieldHintText: "Search Here",
+      choiceChipLabel: (item) {
+        return item!.name;
+      },
+      validateSelectedItem: (list, val) {
+        return list!.contains(val);
+      },
+
+      onItemSearch: (list, text) {
+        if (list != null) {
+          if (list.any((element) =>
+              element.name!.toLowerCase().contains(text.toLowerCase()))) {
+            /// return list which contains matches
+            return list
+                .where((element) =>
+                    element.name!.toLowerCase().contains(text.toLowerCase()))
+                .toList();
+          }
+        }
+
+        return [];
+      },
+
+      onApplyButtonClick: (list) {
+        setState(() {
+          selectedUserList = List.from(list!);
+        });
+        Navigator.pop(context);
+      },
+
+      /// uncomment below code to create custom choice chip
+      /* choiceChipBuilder: (context, item, isSelected) {
+          return Container(
+            padding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+            margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+            decoration: BoxDecoration(
+                border: Border.all(
+              color: isSelected! ? Colors.blue[300]! : Colors.grey[300]!,
+            )),
+            child: Text(
+              item.name,
+              style: TextStyle(
+                  color: isSelected ? Colors.blue[300] : Colors.grey[300]),
+            ),
+          );
+      },*/
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text(widget.title!),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      bottomNavigationBar: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          TextButton(
+            onPressed: () async {
+              var list = await Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => FilterPage(
+                    allTextList: userList,
+                    selectedUserList: selectedUserList,
+                  ),
+                ),
+              );
+              if (list != null) {
+                setState(() {
+                  selectedUserList = List.from(list);
+                });
+              }
+            },
+            child: Text(
+              "Filter Page",
+              style: TextStyle(color: Colors.white),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.blue)),
+          ),
+          TextButton(
+            onPressed: _openFilterDialog,
+            child: Text(
+              "Filter Dialog",
+              style: TextStyle(color: Colors.white),
             ),
-          ],
-        ),
+            style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.blue)),
+            // color: Colors.blue,
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: Column(
+        children: <Widget>[
+          selectedUserList == null || selectedUserList!.length == 0
+              ? Expanded(
+                  child: Center(
+                    child: Text('No text selected'),
+                  ),
+                )
+              : Expanded(
+                  child: ListView.separated(
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          title: Text(selectedUserList![index].name!),
+                        );
+                      },
+                      separatorBuilder: (context, index) => Divider(),
+                      itemCount: selectedUserList!.length),
+                ),
+        ],
+      ),
     );
   }
 }
+
+class FilterPage extends StatelessWidget {
+  const FilterPage({Key? key, this.allTextList, this.selectedUserList})
+      : super(key: key);
+  final List<User>? allTextList;
+  final List<User>? selectedUserList;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Filter list Page"),
+      ),
+      body: SafeArea(
+        child: FilterListWidget<User>(
+          listData: userList,
+          selectedListData: selectedUserList,
+          hideHeaderText: true,
+          onApplyButtonClick: (list) {
+            Navigator.pop(context, list);
+          },
+          choiceChipLabel: (item) {
+            /// Used to print text on chip
+            return item!.name;
+          },
+          choiceChipBuilder: (context, item, isSelected) {
+            return Container(
+              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+              margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+              decoration: BoxDecoration(
+                  border: Border.all(
+                color: isSelected! ? Colors.blue[300]! : Colors.grey[300]!,
+              )),
+              child: Text(item.name),
+            );
+          },
+          validateSelectedItem: (list, val) {
+            ///  identify if item is selected or not
+            return list!.contains(val);
+          },
+          onItemSearch: (list, text) {
+            /// When text change in search text field then return list containing that text value
+            ///
+            ///Check if list has value which matchs to text
+            if (list!.any((element) =>
+                element.name!.toLowerCase().contains(text.toLowerCase()))) {
+              /// return list which contains matches
+              return list
+                  .where((element) =>
+                      element.name!.toLowerCase().contains(text.toLowerCase()))
+                  .toList();
+            }
+            return [];
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class User {
+  final String? name;
+  final String? avatar;
+  User({this.name, this.avatar});
+}
+
+/// Creating a global list for example purpose.
+/// Generally it should be within data class or where ever you want
+List<User> userList = [
+  User(name: "Jon", avatar: "user.png"),
+  User(name: "Lindsey ", avatar: "user.png"),
+  User(name: "Valarie ", avatar: "user.png"),
+  User(name: "Elyse ", avatar: "user.png"),
+  User(name: "Ethel ", avatar: "user.png"),
+  User(name: "Emelyan ", avatar: "user.png"),
+  User(name: "Catherine ", avatar: "user.png"),
+  User(name: "Stepanida  ", avatar: "user.png"),
+  User(name: "Carolina ", avatar: "user.png"),
+  User(name: "Nail  ", avatar: "user.png"),
+];
+
+/// Another exmaple of [FilterListWidget] to filter list of strings
+///
+/// FilterListWidget<String>(
+///   listData: ["One", "Two", "Three", "Four","five","Six","Seven","Eight","Nine","Ten"],
+///   selectedListData: ["One", "Three", "Four","Eight","Nine"],
+///   hideHeaderText: true,
+///   height: MediaQuery.of(context).size.height,
+///   // hideHeaderText: true,
+///   onApplyButtonClick: (list) {
+///     Navigator.pop(context, list);
+///   },
+///   choiceChipLabel: (item) {
+///     /// Used to print text on chip
+///     return item;
+///   },
+///   validateSelectedItem: (list, val) {
+///     ///  identify if item is selected or not
+///     return list!.contains(val);
+///   },
+///   onItemSearch: (list, text) {
+///     /// When text change in search text field then return list containing that text value
+///     ///
+///     ///Check if list has value which matchs to text
+///     if (list!.any((element) =>
+///         element.toLowerCase().contains(text.toLowerCase()))) {
+///       /// return list which contains matches
+///       return list
+///           .where((element) =>
+///               element.toLowerCase().contains(text.toLowerCase()))
+///           .toList();
+///     }
+///     return [];
+///   },
+/// )
